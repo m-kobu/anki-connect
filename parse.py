946605196f6d7af16a55b4327986f1967268ebea
mrcnkobu <mrcnkobu@gmail.com>
2023-06-07 19:39:46 +0200

    Added reading from json, output as a dictionary

--- parse.py
+++ parse.py
@@ -1,13 +1,29 @@
 import sys
 import re
+import json
 
 filename = sys.argv[1]
 
+# toDo: to read lines of variable number of elements in a line
 with open(filename) as file:
+    meta = file.readline()
+    if meta[0] == "#":
+        name = meta.strip(" #\n")
+        print(name)
+        with open(name+".json") as jsonfile:
+            data = json.load(jsonfile)
+            for i in data["fields"]:
+                print(i)
     for line in file:
-        if not line[0]=="#" and line.strip():
+        if line.strip():
             args = re.split(r"\s[-]\s", line.strip())
-            if len(args) == 2:
-                command = args[0].strip(' `')
-                action = args[1].strip()
-                print("command: {} action: {}".format(command, action))
+            x = dict(zip(data["fields"], args))
+            print(x)
+
+#            if len(args) == 2:
+#                command = args[0].strip(' `')
+#                action = args[1].strip()
+#                print("command: {} action: {}".format(command, action))
+
+# the output of that file needs to be in the form [{elements in the line}]
+                
